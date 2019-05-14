f11919e86c72af4dfd5e69550b3053a433b636fe
Loup Vaillant <loup@loup-vaillant.fr>
2019-05-14 02:41:50 +0200

    More fixes

--- monokex.c
+++ monokex.c
@@ -8,20 +8,23 @@
 #define WIPE_CTX(ctx)        crypto_wipe(ctx   , sizeof(*(ctx)))
 #define WIPE_BUFFER(buffer)  crypto_wipe(buffer, sizeof(buffer))
 
+// Message token bytecode
 #define E   1
 #define S   2
 #define EE  3
 #define ES  4
 #define SE  5
 #define SS  6
-#define HAS_KEY      1
-#define HAS_REMOTE   2
-#define GETS_REMOTE  4
-#define GAVE_REMOTE  8
-#define SHOULD_SEND 16
-#define IS_DONE     32
+
+// Context status flags
+#define IS_OK        1 // Allways 1 (becomes zero when wiped)
+#define HAS_KEY      2 // True if we have a symmetric key
+#define HAS_REMOTE   4 // True if we have the remote DH key
+#define GETS_REMOTE  8 // True if the remote key is transmitted to us
+#define SHOULD_SEND 16 // Send/receive toggle
 
 typedef uint8_t u8;
+typedef unsigned short ushort;
 
 static void copy16(u8 out[16], const u8 in[16]){ FOR(i, 0, 16) out[i] = in[i]; }
 static void copy32(u8 out[32], const u8 in[32]){ FOR(i, 0, 32) out[i] = in[i]; }
@@ -36,12 +39,14 @@ static void encrypt(u8 *out, const u8 *in, size_t size, const u8 key[32])
     WIPE_CTX(&ctx);
 }
 
-static int kex_must_give_remote(crypto_kex_ctx *ctx)
-{
-    return (ctx->flags & GETS_REMOTE)
-        && (ctx->flags & HAS_REMOTE)
-        && !(ctx->flags & GAVE_REMOTE);
-}
+typedef crypto_kex_ctx ck_ctx;
+static void kex_set        (ck_ctx*ctx,ushort f){ ctx->flags |=  f;     }
+static void kex_clear      (ck_ctx*ctx,ushort f){ ctx->flags &= ~f;     }
+static int  kex_is_ok      (ck_ctx *ctx) { return ctx->flags & IS_OK;       }
+static int  kex_has_key    (ck_ctx *ctx) { return ctx->flags & HAS_KEY;     }
+static int  kex_has_remote (ck_ctx *ctx) { return ctx->flags & HAS_REMOTE;  }
+static int  kex_gets_remote(ck_ctx *ctx) { return ctx->flags & GETS_REMOTE; }
+static int  kex_should_send(ck_ctx *ctx) { return ctx->flags & SHOULD_SEND; }
 
 /////////////////////
 /// State machine ///
@@ -63,22 +68,36 @@ static void kex_update_key(crypto_kex_ctx *ctx,
     u8 tmp[32];
     crypto_x25519(tmp, secret_key, public_key);
     kex_mix_hash(ctx, tmp, 32);
-    ctx->flags |= HAS_KEY;
+    kex_set(ctx, HAS_KEY);
     WIPE_BUFFER(tmp);
 }
 
 static void kex_send(crypto_kex_ctx *ctx, u8 msg[32], const u8 src[32])
 {
     copy32(msg, src);
-    if (ctx->flags & HAS_KEY) { xor32(msg, ctx->hash + 32); }
+    if (kex_has_key(ctx)) { xor32(msg, ctx->hash + 32); }
     kex_mix_hash(ctx, msg, 32);
 }
 
 static void kex_receive(crypto_kex_ctx *ctx, u8 dest[32], const u8 msg[32])
 {
     copy32(dest, msg);
-    if (ctx->flags & HAS_KEY) { xor32(dest, ctx->hash + 32); }
+    if (kex_has_key(ctx)) { xor32(dest, ctx->hash + 32); }
     kex_mix_hash(ctx, msg, 32);
+}
+
+static unsigned kex_next_token(crypto_kex_ctx *ctx)
+{
+    unsigned token = ctx->messages[0] & 7;
+    ctx->messages[0] >>= 3;
+    return token;
+}
+
+static void kex_next_message(crypto_kex_ctx *ctx)
+{
+    FOR (i, 0, 3) {
+        ctx->messages[i] = ctx->messages[i+1];
+    }
 }
 
 //////////////////////
@@ -87,7 +106,7 @@ static void kex_receive(crypto_kex_ctx *ctx, u8 dest[32], const u8 msg[32])
 static void kex_init(crypto_kex_ctx *ctx, const u8 pid[32])
 {
     copy32(ctx->hash, pid);
-    ctx->flags = 0;
+    ctx->flags = IS_OK; // wiping the context sets it to false
 }
 
 static void kex_seed(crypto_kex_ctx *ctx, u8 random_seed[32])
@@ -121,25 +140,17 @@ int crypto_kex_receive(crypto_kex_ctx *ctx,
     return crypto_kex_receive_p(ctx, 0, 0, message, message_size);
 }
 
-static unsigned kex_next_token(crypto_kex_ctx *ctx)
-{
-    unsigned token = ctx->messages[0] & 7;
-    ctx->messages[0] >>= 3;
-    return token;
-}
-
 void crypto_kex_send_p(crypto_kex_ctx *ctx,
                        u8       *m, size_t m_size,
                        const u8 *p, size_t p_size)
 {
     // Fail if we should not send (the failure is alas delayed)
-    if (!crypto_kex_should_send(ctx) || kex_must_give_remote(ctx)) {
+    if (!crypto_kex_should_send(ctx)) {
         WIPE_CTX(ctx);
         return;
     }
-
-    // flip sending flag
-    ctx->flags ^= SHOULD_SEND;
+    // Next time, we'll receive
+    kex_clear(ctx, SHOULD_SEND);
 
     // Send core message
     while (ctx->messages[0] != 0) { // message not yet empty
@@ -153,20 +164,18 @@ void crypto_kex_send_p(crypto_kex_ctx *ctx,
         default:; // never happens
         }
     }
-    // Prepare for next message (if any)
-    FOR (i, 0, 3) { ctx->messages[i] = ctx->messages[i+1]; }
-    if (ctx->messages[0] == 0) { ctx->flags |= IS_DONE; }
+    kex_next_message(ctx);
 
     // Send payload
     if (p != 0) {
-        if (ctx->flags & HAS_KEY) { encrypt(m, p, p_size, ctx->hash + 32); }
-        else                      { FOR(i, 0, p_size) { m[i] = p[i]; }     }
+        if (kex_has_key(ctx)) { encrypt(m, p, p_size, ctx->hash + 32); }
+        else                  { FOR(i, 0, p_size) { m[i] = p[i]; }     }
         kex_mix_hash(ctx, m, p_size);
         m      += p_size;
         m_size -= p_size;
     }
     // Authenticate
-    if (ctx->flags & HAS_KEY) {
+    if (kex_has_key(ctx)) {
         copy16(m, ctx->hash + 32);
         kex_mix_hash(ctx, 0, 0);
         m      += 16;
@@ -181,19 +190,19 @@ int crypto_kex_receive_p(crypto_kex_ctx *ctx,
                          const u8 *m, size_t m_size)
 {
     // Do nothing & fail if we should not receive
-    if (!crypto_kex_should_receive(ctx) || kex_must_give_remote(ctx)) {
+    if (!crypto_kex_should_receive(ctx)) {
         WIPE_CTX(ctx);
         return -1;
     }
-    // flip sending flag
-    ctx->flags ^= SHOULD_SEND;
+    // Next time, we'll send
+    kex_set(ctx, SHOULD_SEND);
 
     // receive core message
     while (ctx->messages[0] != 0) { // message not yet empty
         switch (kex_next_token(ctx)) {
         case E : kex_receive(ctx, ctx->remote_pke, m); m+=32; m_size-=32; break;
         case S : kex_receive(ctx, ctx->remote_pk , m); m+=32; m_size-=32;
-                 ctx->flags |= HAS_REMOTE;                                break;
+                 kex_set(ctx, HAS_REMOTE);                                break;
         case EE: kex_update_key(ctx, ctx->local_ske, ctx->remote_pke);    break;
         case ES: kex_update_key(ctx, ctx->local_ske, ctx->remote_pk );    break;
         case SE: kex_update_key(ctx, ctx->local_sk , ctx->remote_pke);    break;
@@ -201,16 +210,14 @@ int crypto_kex_receive_p(crypto_kex_ctx *ctx,
         default:; // never happens
         }
     }
-    // Prepare for next message (if any)
-    FOR (i, 0, 3) { ctx->messages[i] = ctx->messages[i+1]; }
-    if (ctx->messages[0] == 0) { ctx->flags |= IS_DONE; }
+    kex_next_message(ctx);
 
     // Take payload into account
     if (p != 0) {
         kex_mix_hash(ctx, m, p_size);
     }
     // Verify (may fail)
-    if (ctx->flags & HAS_KEY) {
+    if (kex_has_key(ctx)) {
         if (crypto_verify16(ctx->hash + 32, m + p_size)) {
             WIPE_CTX(ctx);
             return -1;
@@ -219,14 +226,17 @@ int crypto_kex_receive_p(crypto_kex_ctx *ctx,
     }
     // Receive payload
     if (p != 0) {
-        if (ctx->flags & HAS_KEY) { encrypt(p, m, p_size, ctx->hash + 32); }
-        else                      { FOR(i, 0, p_size) { p[i] = m[i]; }     }
+        if (kex_has_key(ctx)) { encrypt(p, m, p_size, ctx->hash + 32); }
+        else                  { FOR(i, 0, p_size) { p[i] = m[i]; }     }
         m      += p_size;
         m_size -= p_size;
     }
     // Check for size overflow (only possible if we misuse the API)
-    int underflow = m_size >> ((sizeof(m_size) * 8) - 1);
-    return underflow ? -1 : 0;
+    if (m_size >> ((sizeof(m_size) * 8) - 1)) {
+        WIPE_CTX(ctx);
+        return -1;
+    }
+    return 0;
 }
 
 //////////////
@@ -234,27 +244,33 @@ int crypto_kex_receive_p(crypto_kex_ctx *ctx,
 //////////////
 int crypto_kex_has_remote_key(crypto_kex_ctx *ctx)
 {
-    return ctx->flags & HAS_REMOTE;
+    return kex_has_remote(ctx);
 }
 
 int crypto_kex_is_done(crypto_kex_ctx *ctx)
 {
-    return ctx->flags & IS_DONE;
+    return kex_is_ok(ctx)
+        && ctx->messages[0] == 0
+        && (!kex_gets_remote(ctx) || !kex_has_remote(ctx));
 }
 
 int crypto_kex_should_send(crypto_kex_ctx *ctx)
 {
-    return !crypto_kex_is_done(ctx) && (ctx->flags & SHOULD_SEND);
+    return kex_is_ok          (ctx)
+        && kex_should_send    (ctx)
+        && !crypto_kex_is_done(ctx);
 }
 
 int crypto_kex_should_receive(crypto_kex_ctx *ctx)
 {
-    return !crypto_kex_is_done(ctx) && !(ctx->flags & SHOULD_SEND);
+    return kex_is_ok          (ctx)
+        && !kex_should_send   (ctx)
+        && !crypto_kex_is_done(ctx);
 }
 
 size_t crypto_kex_next_message_min_size(crypto_kex_ctx *ctx)
 {
-    unsigned has_key = ctx->flags & HAS_KEY;
+    unsigned has_key = kex_has_key(ctx);
     uint16_t message = ctx->messages[0];
     size_t   size    = 0;
     while (message != 0) {
@@ -262,7 +278,7 @@ size_t crypto_kex_next_message_min_size(crypto_kex_ctx *ctx)
         has_key |= (message & 7) >= 3 ?  1 : 0;
         message >>= 3;
     }
-    return size + has_key ? 16 : 0;
+    return size + (has_key ? 16 : 0);
 }
 
 ////////////////
@@ -270,14 +286,18 @@ size_t crypto_kex_next_message_min_size(crypto_kex_ctx *ctx)
 ////////////////
 void crypto_kex_get_remote_key(crypto_kex_ctx *ctx, uint8_t key[32])
 {
-    if (HAS_REMOTE) { copy32(key, ctx->remote_pk); }
-    ctx->flags |= GAVE_REMOTE;
+    if (!kex_has_remote(ctx)) {
+        WIPE_CTX(ctx);
+        return;
+    }
+    copy32(key, ctx->remote_pk);
+    kex_clear(ctx, GETS_REMOTE);
 }
 
 void crypto_kex_get_session_key(crypto_kex_ctx *ctx,
                                 u8 key[32], u8 extra[32])
 {
-    if (crypto_kex_is_done(ctx) && !kex_must_give_remote(ctx)) {
+    if (crypto_kex_is_done(ctx)) {
         copy32(key, ctx->hash);
         if (extra != 0) {
             copy32(extra, ctx->hash + 32);
