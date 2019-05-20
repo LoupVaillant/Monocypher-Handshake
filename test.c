76989e83c88cdcee6a214e023e21285e68eef1c9
Loup Vaillant <loup@loup-vaillant.fr>
2019-05-20 23:54:08 +0200

    Better payload testing

--- test.c
+++ test.c
@@ -13,14 +13,64 @@ static void check(int condition, const char *error)
     }
 }
 
-static void check_equal(u8 *a, u8 *b, size_t size, const char *error)
+static void check_equal(const u8 *a, const u8 *b, size_t size,
+                        const char *error)
 {
     check(!memcmp(a, b, size), error);
 }
 
 typedef struct {
-    crypto_kex_ctx    ctx;
-    crypto_kex_action action;
+    u8 client_sk  [32];
+    u8 server_sk  [32];
+    u8 client_seed[32];
+    u8 server_seed[32];
+    u8 prelude    [32]; int has_prelude;    size_t prelude_size;
+    u8 payloads[4][32]; int has_payload[4]; size_t payload_size[4];
+} inputs;
+
+void fill_inputs(inputs *i, unsigned nb)
+{
+    p_random(i->client_sk  , 32);
+    p_random(i->server_sk  , 32);
+    p_random(i->client_seed, 32);
+    p_random(i->server_seed, 32);
+    i->prelude_size    = 32;
+    i->payload_size[0] = 32;
+    i->payload_size[1] = 32;
+    i->payload_size[2] = 32;
+    i->payload_size[3] = 32;
+    i->has_prelude     = nb &  1 ? 1 : 0;
+    i->has_payload[0]  = nb &  2 ? 1 : 0;
+    i->has_payload[1]  = nb &  4 ? 1 : 0;
+    i->has_payload[2]  = nb &  8 ? 1 : 0;
+    i->has_payload[3]  = nb & 16 ? 1 : 0;
+    if (i->has_prelude   ) { memset(i->prelude    , 0x33, 32); }
+    if (i->has_payload[0]) { memset(i->payloads[0], 0x44, 32); }
+    if (i->has_payload[1]) { memset(i->payloads[1], 0x55, 32); }
+    if (i->has_payload[2]) { memset(i->payloads[2], 0x66, 32); }
+    if (i->has_payload[3]) { memset(i->payloads[3], 0x77, 32); }
+}
+
+/* void print_inputs(const inputs *i) */
+/* { */
+/*     printf("client_sk  : "); print_vector(i->client_sk  , 32); */
+/*     printf("server_sk  : "); print_vector(i->server_sk  , 32); */
+/*     printf("client_seed: "); print_vector(i->client_seed, 32); */
+/*     printf("server_seed: "); print_vector(i->server_seed, 32); */
+/*     if (i->has_prelude) { */
+/*         printf("prelude    : "); */
+/*         print_vector(i->prelude, 32); */
+/*     } */
+/*     FOR (j, 0, 4) { */
+/*         if (i->has_payload[j]) { */
+/*             printf("payload[%lu]    : ", j); */
+/*             print_vector(i->payloads[j], 32); */
+/*         } */
+/*     } */
+/* } */
+
+typedef struct {
+    crypto_kex_ctx ctx;
     u8 session_key[ 32];
     u8 extra_key  [ 32];
     u8 remote_key [ 32];
@@ -28,6 +78,7 @@ typedef struct {
     u8 messages[4][128];
     unsigned msg_num;
 } handshake_ctx;
+
 
 /* static void print_handshake(handshake_ctx *ctx) */
 /* { */
@@ -44,64 +95,52 @@ typedef struct {
 /*     printf("message 4    ");  print_vector(ctx->messages[3], 128); */
 /* } */
 
-static void step(handshake_ctx *ctx, u8 *msg, u8 *pld, unsigned nb)
+static void step(handshake_ctx *ctx, u8 *msg, const inputs *i)
 {
-    size_t msg_size = crypto_kex_next_message_min_size(&ctx->ctx);
-    ctx->action     = crypto_kex_next_action(&ctx->ctx);
-    switch (ctx->action) {
-    case CRYPTO_KEX_SEND: {
-        pld = nb & (1 << ctx->msg_num) ? pld + ctx->msg_num * 32 : 0;
-        size_t pld_size = pld ? 32 : 0;
-        msg_size += pld_size;
-        crypto_kex_send_p(&ctx->ctx, msg, msg_size, pld, pld_size);
-        memcpy(ctx->messages[ctx->msg_num], msg, msg_size);
-        if (pld) {
+    while (1) {
+        int    has_pld  = i->has_payload[ctx->msg_num];
+        u8     pld_size = has_pld ? i->payload_size[ctx->msg_num] : 0;
+        size_t msg_size = crypto_kex_next_message_min_size(&ctx->ctx)
+                        + pld_size;
+        switch (crypto_kex_next_action(&ctx->ctx)) {
+        case CRYPTO_KEX_SEND: {
+            const u8 *pld = i->payloads[ctx->msg_num];
+            crypto_kex_send_p(&ctx->ctx, msg, msg_size, pld, pld_size);
+            memcpy(ctx->messages[ctx->msg_num], msg, msg_size);
             memcpy(ctx->payloads[ctx->msg_num], pld, pld_size);
+            ctx->msg_num++;
+            return;
         }
-        ctx->msg_num++;
-        return;
-    }
-    case CRYPTO_KEX_RECEIVE: {
-        pld = nb & (1 << ctx->msg_num) ? pld + ctx->msg_num * 32 : 0;
-        size_t pld_size = pld ? 32 : 0;
-        msg_size += pld_size;
-        check(!crypto_kex_receive_p(&ctx->ctx, pld, pld_size, msg, msg_size),
-              "corrupt message");
-        memcpy(ctx->messages[ctx->msg_num], msg, msg_size);
-        if (pld) {
-            memcpy(ctx->payloads[ctx->msg_num], pld, pld_size);
+        case CRYPTO_KEX_RECEIVE: {
+            u8 *pld = ctx->payloads[ctx->msg_num];
+            check(!crypto_kex_receive_p(&ctx->ctx,
+                                        pld, pld_size,
+                                        msg, msg_size),
+                  "corrupt message");
+            memcpy(ctx->messages[ctx->msg_num], msg, msg_size);
+            ctx->msg_num++;
+            continue;
         }
-        ctx->msg_num++;
-        return;
+        case CRYPTO_KEX_GET_REMOTE_KEY:
+            crypto_kex_get_remote_key(&ctx->ctx, ctx->remote_key);
+            continue;
+        case CRYPTO_KEX_GET_SESSION_KEY:
+            crypto_kex_get_session_key(&ctx->ctx,
+                                       ctx->session_key,
+                                       ctx->extra_key);
+            return;
+        default: // CRYPTO_KEX_NOTHING
+            return;
+        }
     }
-    case CRYPTO_KEX_GET_REMOTE_KEY:
-        crypto_kex_get_remote_key(&ctx->ctx, ctx->remote_key);
-        return;
-    case CRYPTO_KEX_GET_SESSION_KEY:
-        crypto_kex_get_session_key(&ctx->ctx, ctx->session_key, ctx->extra_key);
-        return;
-    default: check(0, "One step too many");
-    }
-    return;
-}
-
-static void respond(handshake_ctx *ctx, u8 *msg, u8 *pld, unsigned nb)
-{
-    do {
-        if (ctx->action == CRYPTO_KEX_GET_SESSION_KEY) { break; }
-        step(ctx, msg, pld, nb);
-    }  while (ctx->action != CRYPTO_KEX_SEND &&
-              ctx->action != CRYPTO_KEX_GET_SESSION_KEY);
 }
 
 static void session(handshake_ctx *client_ctx,
                     handshake_ctx *server_ctx,
-                    u8 client_key[32],
-                    u8 server_key[32],
-                    unsigned nb)
+                    const inputs *i,
+                    const u8 client_key[32],
+                    const u8 server_key[32])
 {
-    client_ctx->action  = CRYPTO_KEX_NOTHING;
-    server_ctx->action  = CRYPTO_KEX_NOTHING;
     client_ctx->msg_num = 0;
     server_ctx->msg_num = 0;
     FOR (i, 0, 4) {
@@ -110,24 +149,16 @@ static void session(handshake_ctx *client_ctx,
         memset(client_ctx->payloads[i], 255,  32);
         memset(server_ctx->payloads[i], 255,  32);
     }
-    u8 payloads[128];
-    memset(payloads +  0, 0x33, 32);
-    memset(payloads + 32, 0x44, 32);
-    memset(payloads + 64, 0x55, 32);
-    memset(payloads + 96, 0x66, 32);
-    u8 prelude[32];
-    memset(prelude, 0x77, 32);
-
-    if ((nb & 16) != 0) {
-        crypto_kex_add_prelude(&client_ctx->ctx, prelude, 32);
-        crypto_kex_add_prelude(&server_ctx->ctx, prelude, 32);
+    if (i->has_prelude) {
+        crypto_kex_add_prelude(&client_ctx->ctx, i->prelude, i->prelude_size);
+        crypto_kex_add_prelude(&server_ctx->ctx, i->prelude, i->prelude_size);
     }
 
     u8 msg[128]; // maximum size of messages without 32 bytes payloads
-    while (client_ctx->action != CRYPTO_KEX_GET_SESSION_KEY ||
-           server_ctx->action != CRYPTO_KEX_GET_SESSION_KEY) {
-        respond(client_ctx, msg, payloads, nb);
-        respond(server_ctx, msg, payloads, nb);
+    while (crypto_kex_next_action(&client_ctx->ctx) != CRYPTO_KEX_NOTHING ||
+           crypto_kex_next_action(&server_ctx->ctx) != CRYPTO_KEX_NOTHING) {
+        step(client_ctx, msg, i);
+        step(server_ctx, msg, i);
     }
 
     /* printf("Client handshake\n"); */
@@ -158,42 +189,42 @@ static void session(handshake_ctx *client_ctx,
 
 static void xk1_session(unsigned nb)
 {
-    RANDOM_INPUT(client_sk, 32);
-    RANDOM_INPUT(server_sk, 32);
-    RANDOM_INPUT(client_seed, 32);
-    RANDOM_INPUT(server_seed, 32);
-    u8 client_pk[32];  crypto_key_exchange_public_key(client_pk, client_sk);
-    u8 server_pk[32];  crypto_key_exchange_public_key(server_pk, server_sk);
+    inputs i;
+    fill_inputs(&i, nb);
+    u8 client_pk  [32];  crypto_key_exchange_public_key(client_pk, i.client_sk);
+    u8 server_pk  [32];  crypto_key_exchange_public_key(server_pk, i.server_sk);
+    u8 client_seed[32];  memcpy(client_seed, i.client_seed, 32);
+    u8 server_seed[32];  memcpy(server_seed, i.server_seed, 32);
 
     handshake_ctx client_ctx;
     crypto_kex_xk1_init_client(&client_ctx.ctx, client_seed,
-                               client_sk, client_pk, server_pk);
+                               i.client_sk, client_pk, server_pk);
     memcpy(client_ctx.remote_key, server_pk, 32);
 
     handshake_ctx server_ctx;
     crypto_kex_xk1_init_server(&server_ctx.ctx, server_seed,
-                               server_sk, server_pk);
+                               i.server_sk, server_pk);
 
-    session(&client_ctx, &server_ctx, client_pk, server_pk, nb);
+    session(&client_ctx, &server_ctx, &i, client_pk, server_pk);
 }
 
 void x_session(unsigned nb)
 {
-    RANDOM_INPUT(client_sk, 32);
-    RANDOM_INPUT(server_sk, 32);
-    RANDOM_INPUT(client_seed, 32);
-    u8 client_pk[32];  crypto_key_exchange_public_key(client_pk, client_sk);
-    u8 server_pk[32];  crypto_key_exchange_public_key(server_pk, server_sk);
+    inputs i;
+    fill_inputs(&i, nb);
+    u8 client_pk  [32];  crypto_key_exchange_public_key(client_pk, i.client_sk);
+    u8 server_pk  [32];  crypto_key_exchange_public_key(server_pk, i.server_sk);
+    u8 client_seed[32];  memcpy(client_seed, i.client_seed, 32);
 
     handshake_ctx client_ctx;
     crypto_kex_x_init_client(&client_ctx.ctx, client_seed,
-                             client_sk, client_pk, server_pk);
+                             i.client_sk, client_pk, server_pk);
     memcpy(client_ctx.remote_key, server_pk, 32);
 
     handshake_ctx server_ctx;
-    crypto_kex_x_init_server(&server_ctx.ctx, server_sk, server_pk);
+    crypto_kex_x_init_server(&server_ctx.ctx, i.server_sk, server_pk);
 
-    session(&client_ctx, &server_ctx, client_pk, server_pk, nb);
+    session(&client_ctx, &server_ctx, &i, client_pk, server_pk);
 }
 
 int main()
