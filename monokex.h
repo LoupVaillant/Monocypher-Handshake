667dad1b8f3d9e0be1d3dccc97714354f1333b80
Loup Vaillant <loup@loup-vaillant.fr>
2019-05-29 22:14:28 +0200

    More comments in the header

--- monokex.h
+++ monokex.h
@@ -21,18 +21,28 @@ typedef enum {
     CRYPTO_KEX_NONE,
 } crypto_kex_action;
 
-// Basic send & receive functions
+// Basic read & write functions
 // Maximum message size is 96 bytes
+//
 // If message_size is bigger than the actual message, the message will
 // be padded with zeroes.
+//
+// If message_size is smaller than the actual message, the behaviour is
+// undefined.  (The implementation tries to fail loudly, though)
+//
 // Padding bytes are ignored by crypto_kex_read().
+//
 int  crypto_kex_read(crypto_kex_ctx *ctx,
                      const uint8_t *message, size_t message_size);
 void crypto_kex_write(crypto_kex_ctx *ctx,
                       uint8_t *message, size_t message_size);
 
-// Advanced send & receive functions (with payload)
+// Advanced read & write functions (with payload)
 // Maximum message size is 96 bytes, plus the size of the payload.
+//
+// If payload is NULL, no payload is sent. Payload_size must be zero.
+// If payload_size is zero, but payload is not NULL, an empty payload is
+// sent.
 int  crypto_kex_read_p(crypto_kex_ctx *ctx,
                        uint8_t       *payload, size_t payload_size,
                        const uint8_t *message, size_t message_size);
@@ -57,13 +67,16 @@ void crypto_kex_final(crypto_kex_ctx *ctx,
                       uint8_t session_key[32],
                       uint8_t extra_key  [32]);
 
-// Next action to perform.
+// Next action to perform. Can be used instead of hard coding everything.
 //
 // CRYPTO_KEX_READ        call crypto_kex_read()
 // CRYPTO_KEX_WRITE       call crypto_kex_write()
 // CRYPTO_KEX_REMOTE_KEY  call crypto_kex_remote_key()
 // CRYPTO_KEX_FINAL       call crypto_kex_final()
 // CRYPTO_KEX_NONE        The context has been wiped, don't call anything.
+//
+// If next_message_size is not NULL, the minimum size of the next
+// message (without payload) will be written in it.
 crypto_kex_action crypto_kex_next_action(const crypto_kex_ctx *ctx,
                                          size_t *next_message_size);
 
@@ -71,12 +84,28 @@ crypto_kex_action crypto_kex_next_action(const crypto_kex_ctx *ctx,
 ///////////
 /// XK1 ///
 ///////////
+
+// Initialises a handshake context for the client.
+// Actions happen in the following order:
+//
+// - write (32 bytes)
+// - read  (48 bytes)
+// - write (64 bytes)
+// - final
 void crypto_kex_xk1_client_init(crypto_kex_ctx *ctx,
                                 uint8_t         random_seed[32],
                                 const uint8_t   client_sk  [32],
                                 const uint8_t   client_pk  [32],
                                 const uint8_t   server_pk  [32]);
 
+// Initialises a handshake context for the server.
+// Actions happen in the following order:
+//
+// - read  (32 bytes)
+// - write (48 bytes)
+// - read  (64 bytes)
+// - remote key
+// - final
 void crypto_kex_xk1_server_init(crypto_kex_ctx *ctx,
                                 uint8_t         random_seed[32],
                                 const uint8_t   server_sk  [32],
@@ -85,12 +114,24 @@ void crypto_kex_xk1_server_init(crypto_kex_ctx *ctx,
 /////////
 /// X ///
 /////////
+
+// Initialises a handshake context for the client.
+// Actions happen in the following order:
+//
+// - write (96 bytes)
+// - final
 void crypto_kex_x_client_init(crypto_kex_ctx *ctx,
                               uint8_t         random_seed[32],
                               const uint8_t   client_sk  [32],
                               const uint8_t   client_pk  [32],
                               const uint8_t   server_pk  [32]);
 
+// Initialises a handshake context for the server.
+// Actions happen in the following order:
+//
+// - read  (96 bytes)
+// - remote key
+// - final
 void crypto_kex_x_server_init(crypto_kex_ctx *ctx,
                               const uint8_t   server_sk [32],
                               const uint8_t   server_pk [32]);
