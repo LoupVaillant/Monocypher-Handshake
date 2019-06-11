2ba71ef2c6afeaf178817ad62c97a84e9c0f6a7d
Loup Vaillant <loup@loup-vaillant.fr>
2019-06-12 00:17:31 +0200

    Updated speed.c (renamed API)

--- speed.c
+++ speed.c
@@ -69,41 +69,41 @@ static void get_interactive_session(u8 msg1[32], u8 msg2[48], u8 msg3[64],
         s_seed[i] = server_seed[i];
     }
     crypto_kex_ctx client_ctx;
-    crypto_kex_xk1_init_client(&client_ctx, c_seed, client_sk, client_pk,
+    crypto_kex_xk1_client_init(&client_ctx, c_seed, client_sk, client_pk,
                                server_pk);
     crypto_kex_ctx server_ctx;
-    crypto_kex_xk1_init_server(&server_ctx, s_seed, server_sk, server_pk);
+    crypto_kex_xk1_server_init(&server_ctx, s_seed, server_sk, server_pk);
 
-    crypto_kex_send       (&client_ctx, msg1, 32);
-    if (crypto_kex_receive(&server_ctx, msg1, 32)) {
+    crypto_kex_write   (&client_ctx, msg1, 32);
+    if (crypto_kex_read(&server_ctx, msg1, 32)) {
         fprintf(stderr, "msg1 corrupted\n");
         return;
     }
-    crypto_kex_send       (&server_ctx, msg2, 48);
-    if (crypto_kex_receive(&client_ctx, msg2, 48)) {
+    crypto_kex_write   (&server_ctx, msg2, 48);
+    if (crypto_kex_read(&client_ctx, msg2, 48)) {
         fprintf(stderr, "msg2 corrupted\n");
         return;
     }
-    crypto_kex_send       (&client_ctx, msg3, 64);
-    if (crypto_kex_receive(&server_ctx, msg3, 64)) {
+    crypto_kex_write   (&client_ctx, msg3, 64);
+    if (crypto_kex_read(&server_ctx, msg3, 64)) {
         fprintf(stderr, "msg3 corrupted\n");
         return;
     }
 
     u8 remote_pk[32]; // same as client_pk
-    crypto_kex_get_remote_key(&server_ctx, remote_pk);
+    crypto_kex_remote_key(&server_ctx, remote_pk);
 
     u8 client_session_key1[32];
     u8 client_session_key2[32];
-    crypto_kex_get_session_key(&client_ctx,
-                               client_session_key1,
-                               client_session_key2);
+    crypto_kex_final(&client_ctx,
+                     client_session_key1,
+                     client_session_key2);
 
     u8 server_session_key1[32];
     u8 server_session_key2[32];
-    crypto_kex_get_session_key(&server_ctx,
-                               server_session_key1,
-                               server_session_key2);
+    crypto_kex_final(&server_ctx,
+                     server_session_key1,
+                     server_session_key2);
 
     if (crypto_verify32(client_session_key1, server_session_key1) ||
         crypto_verify32(client_session_key2, server_session_key2)) {
@@ -135,14 +135,14 @@ static u64 interactive_client(void)
         FOR (i, 0, 32) {
             seed[i] = client_seed[i];
         }
-        crypto_kex_xk1_init_client(&client_ctx, seed, client_sk, client_pk,
+        crypto_kex_xk1_client_init(&client_ctx, seed, client_sk, client_pk,
                                    server_pk);
-        crypto_kex_send(&client_ctx, msg1, 32);
-        if (crypto_kex_receive(&client_ctx, msg2, 48)) {
+        crypto_kex_write   (&client_ctx, msg1, 32);
+        if (crypto_kex_read(&client_ctx, msg2, 48)) {
             fprintf(stderr, "msg2 corrupted\n");
             return 1;
         }
-        crypto_kex_send(&client_ctx, msg3, 64);
+        crypto_kex_write(&client_ctx, msg3, 64);
     }
     TIMING_END;
 }
@@ -165,14 +165,14 @@ static u64 interactive_server(void)
         FOR (i, 0, 32) {
             seed[i] = server_seed[i];
         }
-        crypto_kex_xk1_init_server(&server_ctx, seed, server_sk, server_pk);
+        crypto_kex_xk1_server_init(&server_ctx, seed, server_sk, server_pk);
 
-        if (crypto_kex_receive(&server_ctx, msg1, 32)) {
+        if (crypto_kex_read(&server_ctx, msg1, 32)) {
             fprintf(stderr, "msg1 corrupted\n");
             return 1;
         }
-        crypto_kex_send       (&server_ctx, msg2, 48);
-        if (crypto_kex_receive(&server_ctx, msg3, 64)) {
+        crypto_kex_write   (&server_ctx, msg2, 48);
+        if (crypto_kex_read(&server_ctx, msg3, 64)) {
             fprintf(stderr, "msg3 corrupted\n");
             return 1;
         }
@@ -194,31 +194,31 @@ static void get_one_way_session(u8 msg[96], u8 client_pk[32], u8 server_pk[32],
     }
 
     crypto_kex_ctx client_ctx;
-    crypto_kex_x_init_client(&client_ctx, c_seed, client_sk, client_pk,
+    crypto_kex_x_client_init(&client_ctx, c_seed, client_sk, client_pk,
                              server_pk);
     crypto_kex_ctx server_ctx;
-    crypto_kex_x_init_server(&server_ctx, server_sk, server_pk);
+    crypto_kex_x_server_init(&server_ctx, server_sk, server_pk);
 
-    crypto_kex_send       (&client_ctx, msg, 96);
-    if (crypto_kex_receive(&server_ctx, msg, 96)) {
+    crypto_kex_write   (&client_ctx, msg, 96);
+    if (crypto_kex_read(&server_ctx, msg, 96)) {
         fprintf(stderr, "msg corrupted\n");
         return;
     }
 
     u8 remote_pk[32]; // same as client_pk
-    crypto_kex_get_remote_key(&server_ctx, remote_pk);
+    crypto_kex_remote_key(&server_ctx, remote_pk);
 
     u8 client_session_key1[32];
     u8 client_session_key2[32];
-    crypto_kex_get_session_key(&client_ctx,
-                               client_session_key1,
-                               client_session_key2);
+    crypto_kex_final(&client_ctx,
+                     client_session_key1,
+                     client_session_key2);
 
     u8 server_session_key1[32];
     u8 server_session_key2[32];
-    crypto_kex_get_session_key(&server_ctx,
-                               server_session_key1,
-                               server_session_key2);
+    crypto_kex_final(&server_ctx,
+                     server_session_key1,
+                     server_session_key2);
 
     if (crypto_verify32(client_session_key1, server_session_key1) ||
         crypto_verify32(client_session_key2, server_session_key2)) {
@@ -247,10 +247,10 @@ static u64 one_way_client(void)
             seed[i] = client_seed[i];
         }
         crypto_kex_ctx client_ctx;
-        crypto_kex_x_init_client(&client_ctx, seed, client_sk, client_pk,
+        crypto_kex_x_client_init(&client_ctx, seed, client_sk, client_pk,
                                  server_pk);
 
-        crypto_kex_send(&client_ctx, msg, 96);
+        crypto_kex_write(&client_ctx, msg, 96);
     }
     TIMING_END;
 }
@@ -267,8 +267,8 @@ static u64 one_way_server(void)
                         client_seed);
     TIMING_START {
         crypto_kex_ctx server_ctx;
-        crypto_kex_x_init_server(&server_ctx, server_sk, server_pk);
-        if (crypto_kex_receive(&server_ctx, msg, 96)) {
+        crypto_kex_x_server_init(&server_ctx, server_sk, server_pk);
+        if (crypto_kex_read(&server_ctx, msg, 96)) {
             fprintf(stderr, "msg corrupted\n");
             return 1;
         }
